aca0b3b1a36d1d5d628588aa7f284b5636a737fa
Matthias <matthias-endler@gmx.net>
2021-02-15 10:56:17 +0100

    Formatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@ set -uxo pipefail
 
 LYCHEE_OUT=${LYCHEE_OUT:="lychee/out.md"}
 LYCHEE_TMP="/tmp/lychee/out.md"
-GITHUB_WORKFLOW_URL=https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID 
-
+GITHUB_WORKFLOW_URL="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 
 # Create temp dir
 mkdir -p "$(dirname $LYCHEE_TMP)"
@@ -20,7 +19,7 @@ if [ $exit_code -ne 0 ]; then
     echo >> $LYCHEE_OUT
     cat "$LYCHEE_TMP" >> $LYCHEE_OUT
     echo >> $LYCHEE_OUT
-    echo "[Full Github Action output](${GITHUB_WORKFLOW_URL})" >> $LYCHEE_OUT
+    echo "[Full Github Actions output](${GITHUB_WORKFLOW_URL})" >> $LYCHEE_OUT
 fi
 
 # Output to console
@@ -28,4 +27,3 @@ cat "$LYCHEE_TMP"
 
 # Pass lychee exit code to next step
 echo ::set-output name=exit_code::$exit_code
-
