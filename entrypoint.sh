f2c87a7b30557a578e857c8d5634db77466eb337
Matthias <matthias-endler@gmx.net>
2021-02-15 10:25:49 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -uxo pipefail
 
 LYCHEE_OUT=${LYCHEE_OUT:="lychee/out.md"}
 LYCHEE_TMP="/tmp/lychee/out.md"
+GITHUB_WORKFLOW_URL=https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID 
+
 
 # Create temp dir
 mkdir -p "$(dirname $LYCHEE_TMP)"
@@ -14,8 +16,11 @@ exit_code=$?
 # If link errors were found, create a report in the designated directory
 if [ $exit_code -ne 0 ]; then
     mkdir -p "$(dirname $LYCHEE_OUT)"
-    echo 'Errors were reported while checking the availability of links:' > $LYCHEE_OUT
+    echo 'Errors were reported while checking the availability of links.' > $LYCHEE_OUT
+    echo >> $LYCHEE_OUT
     cat "$LYCHEE_TMP" >> $LYCHEE_OUT
+    echo >> $LYCHEE_OUT
+    echo "${GITHUB_WORKFLOW_URL}" >> $LYCHEE_OUT
 fi
 
 # Output to console
